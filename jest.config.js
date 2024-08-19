ca27bb3724d70911cfd09b5eca797cd3381cb6fc
hashfunc <root@hashfunc.io>
2024-08-20 01:31:12 +0900

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  clearMocks: true,
+  coverageProvider: "v8",
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/setup.js"],
+  testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/**/*.ts",
+    "**/?(*.)+(spec|test).ts"
+  ],
+  testPathIgnorePatterns: ["/node_modules/", ".d.ts", ".js"],
+};
