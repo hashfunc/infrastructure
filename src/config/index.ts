750e3ca7e0534372161ac05971916bebcbb0b83c
hashfunc <root@hashfunc.io>
2024-10-19 19:28:56 +0900

    feat: Add config loader

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from "node:fs";
+import { parse } from "yaml";
+
+import type { NetworkConfig } from "../network/config";
+
+interface Conifg {
+  network: { [name: string]: NetworkConfig };
+}
+
+export function loadConfig(): Conifg {
+  const config = readFileSync("config.yaml", "utf8");
+  return parse(config);
+}
